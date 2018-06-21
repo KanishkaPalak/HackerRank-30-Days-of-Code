d991e87d61e553e5ce3d8576b1bca40bf4611446
Kanishka Rao <kanishka.palak@gmail.com>
2018-06-21 20:26:30 +0530

    Create Day 1: Data Types.cpp

---
+++ Day 1: Data Types.cpp
@@ -0,0 +1,19 @@
+    
+    // Declare second integer, double, and String variables.
+    int a;
+    double b;
+    string c;
+   
+    cin>>a>>b;
+    cin.ignore();
+    getline(cin,c);
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    cout<<i+a<<endl;
+    // Print the sum of the double variables on a new line.
+    printf("%0.1lf",d+b);
+cout<<endl;
+    // Concatenate and print the String variables on a new line
+    cout<<s+c<<endl;
+    // The 's' variable above should be printed first.
